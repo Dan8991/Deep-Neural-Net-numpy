d93d18b9f5f6988bad5b22a944a2af3015e86c4b
Daniele <daniseas98@gmail.com>
2018-09-08 13:28:06 +0200

    [Add]completed the program now the nn trains and then is evaluated on test set

--- src/deepNN.py
+++ src/deepNN.py
@@ -7,7 +7,7 @@ def init_params(layers_shape):
     for i in range(len(layers_shape) - 1):
         W.append(np.random.randn(layers_shape[i + 1], layers_shape[i]) * 0.01)
         b.append(np.zeros([layers_shape[i+1], 1]))
-    return W,b
+    return {"W":W, "b":b}
 
 def relu(x):
     return np.maximum(0, x)
@@ -24,6 +24,28 @@ def one_hot(y):
         res[y[0, i], i] = 1
     return res
 
+def calculate_gradients(X, Y, params, cache):
+    Z1, Z2, Z3 = cache["Z"]
+    A1, A2, A3 = cache["A"]
+    _, W2, W3 = params["W"]
+    m = Y.shape[1]
+
+    dZ3 = A3 - Y
+    dW3 = 1/m*np.dot(dZ3, A2.T)
+    db3 = 1/m*np.sum(dZ3, axis=1, keepdims=True)
+    dZ2 = np.dot(W3.T, dZ3)*(A2 > 0)
+    dW2 = 1/m*np.dot(dZ2, A1.T)
+    db2 = 1/m*np.sum(dZ2, axis=1, keepdims=True)
+    dZ1 = np.dot(W2.T, dZ2)*(A1 > 0)
+    dW1 = 1/m*np.dot(dZ1, X.T)
+    db1 = 1/m*np.sum(dZ1, axis=1, keepdims=True)
+
+    dW = [dW1, dW2, dW3]
+    db = [db1, db2, db3]
+    grads = {"dW":dW, "db":db}
+
+    return grads
+
 def forward_prop(inputs, parameters):
     W1, W2, W3 = parameters["W"]
     b1, b2, b3 = parameters["b"]
@@ -34,20 +56,79 @@ def forward_prop(inputs, parameters):
     A2 = relu(Z2)
     Z3 = np.dot(W3, A2) + b3
     A3 = softmax(Z3)
-    cache = {"Z1":Z1, "Z2":Z2, "Z3":Z3, "A1":A1, "A2":A2, "A3":A3}
+
+    Z = [Z1, Z2, Z3]
+    A = [A1, A2, A3]
+
+    cache = {"Z":Z, "A":A}
     return A3, cache
 
 def loss(y_pred, y):
     m = y.shape[1]
     return -1/m*np.sum(np.sum(y*np.log(y_pred),axis=1), axis=0)
 
+def back_propagate(X, Y, cache, params, learning_rate = 0.01):
+    grads = calculate_gradients(X, Y, params, cache)
+    W1, W2, W3 = params["W"]
+    b1, b2, b3 = params["b"]
+    dW1, dW2, dW3 = grads["dW"]
+    db1, db2, db3 = grads["db"]
+    W1 -= learning_rate*dW1
+    W2 -= learning_rate*dW2
+    W3 -= learning_rate*dW3
+    b1 -= learning_rate*db1
+    b2 -= learning_rate*db2
+    b3 -= learning_rate*db3
+    
+    W = [W1, W2, W3]
+    b = [b1, b2, b3]
+    return {"W":W, "b":b}
+
+def accuracy(y_pred, y):
+    m = y.shape[1]
+    count=0
+    for i in range(m):
+        pred = np.argmax(y_pred[:,i])
+        res = np.argmax(y[:,i])
+        if pred == res:
+            count+=1
+    return count/m
+
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 m, h ,w = x_train.shape
+m_test = x_test.shape[0]
+
+#hyperparameters
+epochs = 100
+batch_size = 32
+alpha = 0.003
+decay_rate = 1
+
+#inputs preprocessing
 x_train = normalize_inputs(np.reshape(x_train, [m, h*w]).T)
 y_train = one_hot(np.reshape(y_train,[1,m]))
+x_test = normalize_inputs(np.reshape(x_test, [m_test, h*w]).T)
+y_test = one_hot(np.reshape(y_test,[1,m_test]))
 
-W, b = init_params([h*w, 128, 128, 10])
-params = {"W": W, "b": b}
-y_pred, cache = forward_prop(x_train, params)
-print(loss(y_pred, y_train))
+params = init_params([h*w, 128, 128, 10])
+
+for i in range(epochs):
+    randomize = np.arange(m)
+    np.random.shuffle(randomize)
+    X = x_train[:, randomize]
+    Y = y_train[:, randomize]
+    for j in range(m//batch_size):
+        X_batch = X[:, batch_size * j:batch_size * (j + 1)]
+        Y_batch = Y[:, batch_size * j:batch_size * (j + 1)]
+        y_pred, cache = forward_prop(X_batch, params)
+        params = back_propagate(X_batch, Y_batch, cache, params, learning_rate=alpha)
+        alpha *= decay_rate 
+    y_pred, _ = forward_prop(x_train, params)
+    l = loss(y_pred, y_train)
+    acc = accuracy(y_pred, y_train)
+    print("epoch: "+str(i)+" loss:"+str(l)+" accuracy:"+str(acc))
+    
+y_pred, _ = forward_prop(x_test, params)
+acc = accuracy(y_pred, y_test)
+print("accuracy on test set:", acc)
